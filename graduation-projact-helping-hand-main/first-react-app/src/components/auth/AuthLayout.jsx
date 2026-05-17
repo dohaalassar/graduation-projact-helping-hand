93d08eec0433dc92af4f497271d3c0f5003e8e58
dohaalassar <dohaalassar2003@gmail.com>
2026-05-17 12:09:49 +0300

    Restructure project: change about header.css and remove LogeBanner

--- graduation-projact-helping-hand-main/first-react-app/src/components/auth/AuthLayout.jsx
+++ graduation-projact-helping-hand-main/first-react-app/src/components/auth/AuthLayout.jsx
@@ -1,5 +1,4 @@
 import Header from "../layout/Header";
-import LogoBanner from "./LogoBanner";
 import FooterNote from "./FooterNote";
 import "../../styles/authlayout.css";
 
@@ -7,7 +6,7 @@ const AuthLayout = ({ children }) => {
   return (
     <div className="auth-bg">
       <Header />
-      <LogoBanner />
+    
 
       <div className="auth-page-content">
         {children}
